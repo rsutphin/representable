47e7bb1a72e9e11a88baf6df36c5d9bba04ff942
Nick Sutterer <apotonick@gmail.com>
2013-07-22 21:06:27 +1000

    make objects and collections in XML work.

--- test/prototype_test.rb
+++ test/prototype_test.rb
@@ -154,6 +154,61 @@ class BllaTest < MiniTest::Spec
     end
   end
 
+  class XMLScalarBinding < JSONObjectBinding
+    def write(parent, value)
+      wrap_node = parent
+
+      #if wrap = options[:wrap]
+      #  parent << wrap_node = node_for(parent, wrap)
+      #end
+
+      wrapped = node_for(parent, from)
+      wrapped.content = serialize(value)
+
+      wrap_node << wrapped
+
+      parent
+    end
+
+  private
+    def node_for(parent, name)
+      Nokogiri::XML::Node.new(name.to_s, parent.document)
+    end
+
+    def serialize(value) # DISCUSS: pass from outside?
+      ScalarRepresenter.new(value, SimplerDefinition.new(@definition, nil), :to_node).serialize # prepare, to_json
+    end
+
+    #def options
+    #  @definition.options
+    #end
+  end
+
+  class XMLObjectBinding < XMLScalarBinding
+    def write(parent, value)
+      parent << serialize(value)
+
+      parent
+    end
+
+    def serialize(value) # DISCUSS: pass from outside?
+      ObjectRepresenter.new(value, SimplerDefinition.new(@definition, nil), :to_node).serialize # prepare, to_json
+    end
+  end
+
+  class XMLCollectionBinding < XMLObjectBinding
+    def write(parent, value)
+      nodes = value.collect { |item| serialize(item) }
+
+      parent << set_for(parent, nodes)
+    end
+
+  private
+    def set_for(parent, nodes)
+      Nokogiri::XML::NodeSet.new(parent.document, nodes)
+    end
+  end
+
   describe "read and write" do
     it { JSONScalarBinding.new(Representable::Definition.new(:title)).write({}, "Kinetic").
       must_equal({"title"=>"Kinetic"}) }
@@ -163,5 +218,22 @@ class BllaTest < MiniTest::Spec
 
     it { JSONCollectionBinding.new(Representable::Definition.new(:songs, :extend => SongRepresenter)).write({}, [song, song]).
       must_equal("songs" => [{"title"=>"Kinetic"},{"title"=>"Kinetic"}]) }
+
+
+
+    it { XMLScalarBinding.new(Representable::Definition.new(:title)).write(Nokogiri::XML::Document.new, "Kinetic").
+      to_s.
+      must_equal_xml "<title>Kinetic</title>" }
+
+    it { XMLObjectBinding.new(Representable::Definition.new(:song, :extend => XMLSongRepresenter)).write(Nokogiri::XML::Document.new, song).
+      to_s.
+      must_equal_xml "<song><title>Kinetic</title></song>" }
+
+    it {
+      root = Nokogiri::XML::Node.new("root", Nokogiri::XML::Document.new)
+
+      XMLCollectionBinding.new(Representable::Definition.new(:songs, :extend => XMLSongRepresenter)).write(root, [song, song]).
+      to_s.
+      must_equal_xml "<root><song><title>Kinetic</title></song><song><title>Kinetic</title></song></root>" }
   end
 end
