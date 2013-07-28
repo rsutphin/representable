26a4c1ef36d495f2c6d1f046fcc259a28d42af61
Nick Sutterer <apotonick@gmail.com>
2013-07-28 22:12:35 +1000

    XML::Collection works.

--- lib/representable/xml/collection.rb
+++ lib/representable/xml/collection.rb
@@ -12,6 +12,18 @@ module Representable::XML
     end
 
     module Methods
+      def create_representation_with(doc, options, format)
+      bin   = representable_mapper(format, options).bindings.first
+
+      if bin.typed?
+          bbin= XMLCollectionBinding.new(bin)
+        else
+         bbin= XMLCollectionBinding.new(bin, XMLScalarBinding)
+       end
+
+       value = bbin.write(doc, represented ) # we need the wrapping here!
+    end
+
       def update_properties_from(doc, options, format)
         node = doc.search("./*") # pass the list of collection items to Hash::Collection#update_properties_from.
         bin   = representable_mapper(format, options).bindings.first
